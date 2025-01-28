9a0e5e0be3a7fd2522c1f2df8b637a9fab2e01d7
Wheatly <156843969+wheatlythe2nd@users.noreply.github.com>
2025-01-28 07:37:33 -0500

    change getpass to msvcrt
    This removes getpass, and implements msvcrt, so that the user has feedback that the password is being input.

--- password-prompt.py
+++ password-prompt.py
@@ -1,33 +1,45 @@
-import getpass
+import msvcrt
 import bcrypt
 
 def hash_password(password):
     """Hash a password using bcrypt."""
-    # Generate a salt and hash the password
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
 
+def get_password(prompt="Enter your password:"):
+    """Prompt for a password, use Windows msvcrt to have password display as astricks."""
+    print(prompt, end='', flush=True)
+    password = []
+    while True:
+        ch = msvcrt.getch()
+        if ch in {b'\r', b'\n'}:  # Enter key
+            print('')
+            break
+        elif ch == b'\x08':  # Backspace key
+            if password:
+                password.pop()
+                print('\b \b', end='', flush=True)
+        else:
+            password.append(ch.decode())
+            print('*', end='', flush=True)
+    return ''.join(password)
+
 def store_password():
-    """Prompt the user to enter a password and store its hash."""
-    password = getpass.getpass('Set your password: ')
+    password = get_password('Set your password: ')
     hashed_password = hash_password(password)
     
-    # Store the hashed password in a file (in a real application, use a secure storage solution)
     with open('password.txt', 'wb') as file:
         file.write(hashed_password)
     
     print('Password stored successfully!')
 
 def verify_password():
-    """Prompt the user to enter the password and verify it."""
-    password = getpass.getpass('Enter your password: ')
+    password = get_password('Enter your password: ')
     
-    # Read the stored hashed password
     with open('password.txt', 'rb') as file:
         stored_hashed_password = file.read()
     
-    # Verify the entered password against the stored hashed password
     if bcrypt.checkpw(password.encode(), stored_hashed_password):
         print('Password is correct!')
     else:
